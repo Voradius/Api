271de7e55c2d8d20704e4c21b63e463d0a4a2565
Stefan Doorn <stefan@efectos.nl>
2015-08-10 16:22:42 +0200

    Add products/search compatible function to Product Entity

--- src/Voradius/Entity/Product.php
+++ src/Voradius/Entity/Product.php
@@ -2,6 +2,7 @@
 
 use Voradius\ClientInterface;
 use Voradius\Exceptions\InvalidParameterException;
+use Voradius\Exceptions\ParameterNotAllowedException;
 use Voradius\Helpers\Url;
 
 /**
@@ -15,6 +16,11 @@ class Product implements EntityInterface
      *
      */
     const PATH = '/v2/products';
+
+    /**
+     * @var array
+     */
+    private $searchWhitelist = ['term', 'sort', 'order', 'category_id', 'lat', 'long', 'shop_id'];
 
     /**
      * @var ClientInterface
@@ -54,6 +60,31 @@ class Product implements EntityInterface
         }
 
         $response = $this->client->getConnection()->get(Url::build(self::PATH, '/ean/' . $ean));
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * @param array $params
+     * @return mixed
+     * @throws InvalidParameterException
+     * @throws ParameterNotAllowedException
+     */
+    public function getSearch(array $params) {
+        if(empty($params)) {
+            throw new InvalidParameterException('Atleast one parameter is required. Choose from: ' . array_keys($this->searchWhitelist));
+        }
+
+        foreach($params as $key => $value) {
+            if(!in_array($key, $this->searchWhitelist)) {
+                throw new ParameterNotAllowedException('Parameter "' . $key . '" not allowed');
+            }
+        }
+
+        if(!array_key_exists('term', $params)) {
+            throw new InvalidParameterException('Parameter "term" is required');
+        }
+
+        $response = $this->client->getConnection()->get(Url::build(self::PATH, 'search', $params));
         return $response->getBody()->getContents();
     }
 
