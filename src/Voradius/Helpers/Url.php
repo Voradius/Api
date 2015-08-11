7d3bfd228acb5fca1feed370ea90c77fdf283ba5
Stefan Doorn <stefan@efectos.nl>
2015-08-11 16:04:22 +0200

    Split up Url Helper

--- src/Voradius/Helpers/Url.php
+++ src/Voradius/Helpers/Url.php
@@ -13,23 +13,71 @@ class Url
      * @param array $params
      * @return string
      */
-    public static function build($base, $path = '', array $params = array()) {
+    public static function build($base, $path = '', array $params = array())
+    {
         // Add path to base (with leading slash if missing)
         if (!empty($path)) {
-            if (strpos($path, '/') !== 0 && substr($base, -1) !== '/') {
-                $base .= '/';
+            if (!self::hasTrailingSlash($base)) {
+                self::addTrailingSlash($base);
             }
 
-            $base .= $path;
+            $base .= self::stripLeadingSlash($path);
         }
 
         // Add parameters
-        if (!empty($params)) {
-            $base .= '?' . http_build_query($params);
-        }
+        $base .= self::createUriParameters($params);
 
         // Return URL
         return $base;
+    }
+
+    /**
+     * @param $str
+     * @return bool
+     */
+    private static function addTrailingSlash(&$str)
+    {
+        $str .= '/';
+        return true;
+    }
+
+    /**
+     * @param array $params
+     * @return string
+     */
+    private static function createUriParameters(array $params)
+    {
+        if (!empty($params)) {
+            return '?' . http_build_query($params);
+        }
+
+        return '';
+    }
+
+    /**
+     * @param $str
+     * @return bool
+     */
+    private static function hasTrailingSlash($str)
+    {
+        if (substr($str, -1) !== '/') {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param $str
+     * @return string
+     */
+    private static function stripLeadingSlash($str)
+    {
+        if (substr($str, 0, 1) === '/') {
+            return substr($str, 1);
+        }
+
+        return $str;
     }
 
 }
